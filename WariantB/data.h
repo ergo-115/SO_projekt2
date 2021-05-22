03357b0f41a1138d53675a51a425c20fb74f3487
vistr <cezaryh1106@gmail.com>
2021-05-22 20:56:10 +0200

    header

--- WariantB/data.h
+++ WariantB/data.h
@@ -10,14 +10,14 @@
 #include <ctype.h>
 
 
-
+//Struktura przechowująca informacje o samochodzie
 typedef struct car {
     pthread_t thread;
     int timesCrossed;
     int state;
     int city;
 } car;
-
+//Struktura przechowująca informacje o moście, czy coś na nim jest
 typedef struct bridgeInfo {
     int carNumber;
     int direction;
